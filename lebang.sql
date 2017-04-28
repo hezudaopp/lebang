a08ccbbac01c69da7ef2bfddad9f9a27c0040364
tangwj <tangwj@273.cn>
2017-04-28 17:54:36 +0800

    sql update

--- lebang.sql
+++ lebang.sql
@@ -5,26 +5,30 @@
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `username` char(64) DEFAULT NULL,
-  `phone` char(11) DEFAULT NULL,
-  `password` char(80) DEFAULT NULL,
-  `all_history_balance` decimal(6,2) DEFAULT NULL,
-  `balance` decimal(6,2) DEFAULT NULL,
-  `channel` varchar(50) DEFAULT NULL,
-  `first_check_time` bigint(20) NOT NULL,
-  `freeze_balance` decimal(6,2) DEFAULT NULL,
-  `imei` varchar(128) DEFAULT NULL,
-  `invite_code` char(5) DEFAULT NULL,
-  `is_valid` tinyint(1) NOT NULL,
-  `valid_time` bigint(20) NOT NULL,
-  `last_login_time` bigint(20) NOT NULL,
-  `reg_ip` bigint(20) NOT NULL,
-  `uuid` varchar(128) DEFAULT NULL,
-  `master_id` bigint(20) DEFAULT NULL,
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `app_id` int(10) unsigned DEFAULT NULL COMMENT '用户来源app',
+  `alias_id` char(64) DEFAULT NULL COMMENT '？？？',
+  `username` varchar(20) NOT NULL COMMENT '用户名',
+  `mobile` char(11) NOT NULL COMMENT '手机号',
+  `password` char(80) DEFAULT NULL COMMENT '密码',
+  `all_history_balance` decimal(6,2) DEFAULT NULL COMMENT '历史全部余额',
+  `balance` decimal(6,2) DEFAULT NULL COMMENT '当前账户余额',
+  `channel` varchar(50) DEFAULT NULL COMMENT '来源渠道',
+  `first_check_time` bigint(20) NOT NULL COMMENT '？？？',
+  `freeze_balance` decimal(6,2) DEFAULT NULL COMMENT '冻结金额',
+  `imei` varchar(128) DEFAULT NULL COMMENT '手机IMEI号',
+  `invite_code` char(5) DEFAULT NULL COMMENT '邀请码',
+  `is_valid` tinyint(1) NOT NULL COMMENT '？？？',
+  `valid_time` bigint(20) NOT NULL COMMENT '？？？',
+  `last_login_time` bigint(20) NOT NULL COMMENT '上次登录时间',
+  `reg_ip` bigint(20) NOT NULL COMMENT '注册ip',
+  `uuid` varchar(128) DEFAULT NULL COMMENT 'uuid',
+  `master_id` bigint(20) DEFAULT NULL COMMENT '？？？',
+  `created_time` int(10) NOT NULL,
+  `modified_time` int(10) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `idx_username` (`username`) USING BTREE,
-  KEY `idx_phone` (`phone`) USING BTREE,
+  KEY `idx_mobile` (`mobile`) USING BTREE,
   KEY `idx_invite_code` (`invite_code`) USING BTREE,
   KEY `idx_master_id` (`master_id`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
@@ -35,8 +39,11 @@ CREATE TABLE `user` (
 -- ----------------------------
 DROP TABLE IF EXISTS `task_type`;
 CREATE TABLE `task_type` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL COMMENT '任务类型名称',
+  `enabled` tinyint(1) NOT NULL COMMENT '是否启用',
+  `created_time` int(10) NOT NULL,
+  `modified_time` int(10) NOT NULL,
   PRIMARY KEY (`id`),
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
@@ -45,7 +52,87 @@ CREATE TABLE `task_type` (
 -- ----------------------------
 DROP TABLE IF EXISTS `task`;
 CREATE TABLE `task` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `task_type_id` bigint(20) unsigned NOT NULL COMMENT '任务类型id',
+  `task_type_name` varchar(100) NOT NULL COMMENT '任务类型名称，冗余',
+  `price` decimal(6,2) unsigned NOT NULL COMMENT '任务单价',
+  `amount` int(10) unsigned NOT NULL COMMENT '任务数量',
+  `left_amount` int(10) unsigned NOT NULL COMMENT '任务余量，冗余',
+  `review_period` int(10) unsigned DEFAULT NULL COMMENT '审核周期',
+  `begin_time` int(10) unsigned NOT NULL COMMENT '任务开始时间',
+  `end_time` int(10) unsigned NOT NULL COMMENT '任务结束时间',
+  `device_type_id` int(10) unsigned NOT NULL COMMENT '设备类型id',
+  `each_person_limit` int(10) unsigned NOT NULL DEFAULT 2147483647 COMMENT '每人限制次数',
+  `recycle_days_limit` int(10) unsigned NOT NULL DEFAULT 2147483647 COMMENT '每次完成限制周期',
+  `recommended_person` varchar(20) DEFAULT NULL COMMENT '推荐人',
+  `max_procedure_num` tinyint(2) unsigned NOT NULL DEFAULT 5 COMMENT '最大允许步骤数',
+  `check_city` tinyint(1) unsigned NOT NULL COMMENT '是否限定城市',
+  `created_time` int(10) unsigned NOT NULL,
+  `modified_time` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- 任务城市表
+-- ----------------------------
+DROP TABLE IF EXISTS `task_cities`;
+CREATE TABLE `task_cities` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `task_id` bigint(20) unsigned NOT NULL COMMENT '任务id',
+  `province_id` bigint(20) unsigned NOT NULL COMMENT '省份id，冗余',
+  `city_id` bigint(20) unsigned NOT NULL COMMENT '城市id',
+  `created_time` int(10) unsigned NOT NULL,
+  `modified_time` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- 任务步骤
+-- ----------------------------
+DROP TABLE IF EXISTS `task_procedure`;
+CREATE TABLE `task_procedure` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `task_id` bigint(20) unsigned NOT NULL COMMENT '任务id',
+  `order` tinyint(2) unsigned NOT NULL COMMENT '顺序',
+  `desc` varchar(2000) NOT NULL COMMENT '描述',
+  `images` varchar(500) NOT NULL COMMENT '图片',
+  `created_time` int(10) unsigned NOT NULL,
+  `modified_time` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- 用户任务
+-- ----------------------------
+DROP TABLE IF EXISTS `user_task`;
+CREATE TABLE `user_task` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) unsigned NOT NULL COMMENT '领取任务的用户id',
+  `task_id` bigint(20) unsigned NOT NULL COMMENT '任务id',
+  `province_id` bigint(20) unsigned DEFAULT NULL COMMENT '省份id',
+  `city_id` bigint(20) unsigned DEFAULT NULL COMMENT '城市id',
+  `note` varchar(2000) NOT NULL COMMENT '用户完成任务留言',
+  `images` varchar(1000) DEFAULT NULL COMMENT '用户完成任务图片',
+  `status` tinyint(2) unsigned NOT NULL COMMENT '任务进度',
+  `end_time` int(10) unsigned NOT NULL DEFAULT 2147483647 COMMENT '任务结束时间，冗余',
+  `reviewer_user_id` bigint(20) unsigned DEFAULT NULL COMMENT '审核用户id',
+  `review_end_time` int(10) unsigned NOT NULL DEFAULT 2147483647 COMMENT '审核截至时间',
+  `created_time` int(10) unsigned NOT NULL,
+  `modified_time` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- 用户任务日志
+-- ----------------------------
+DROP TABLE IF EXISTS `user_task_log`;
+CREATE TABLE `user_task_log` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user_task_id` bigint(20) unsigned NOT NULL COMMENT '用户任务id',
+  `operate_type` tinyint(2) unsigned NOT NULL COMMENT '操作类型',
+  `operator_user` bigint(20) unsigned NOT NULL COMMENT '操作用户',
+  `from_status` tinyint(2) unsigned NOT NULL COMMENT '操作前任务进度',
+  `to_status` tinyint(2) unsigned NOT NULL COMMENT '操作后任务进度',
+  `created_time` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`),
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
