3296abc7aa0e31f6c7d939e0d6a00dea7d6c62e2
Jawinton <Jawinton>
2017-05-13 21:57:11 +0800

    revert user task statistics implements.

--- src/main/java/com/youmayon/lebang/data/UserTaskDao.java
+++ src/main/java/com/youmayon/lebang/data/UserTaskDao.java
@@ -1,12 +1,14 @@
 package com.youmayon.lebang.data;
 
-import com.youmayon.lebang.domain.TaskAppStatistics;
-
 import java.util.List;
 
 /**
  * Created by Jawinton on 17/05/04.
  */
 public interface UserTaskDao {
-    List<TaskAppStatistics> taskAppStatistics(long beginTime, long endTime);
+    List<Object[]> taskAppReceivedAmount(long beginTime, long endTime);
+
+    List<Object[]> taskAppCompletedAmount(long beginTime, long endTime);
+
+    List<Object[]> taskAppAcceptedAmountAndTotalFlow(long beginTime, long endTime);
 }
